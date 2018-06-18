2b952793958ffbdc5cdb1bfc69d5ff0e352a917d
Harinaathgobi <30602947+Harinaathgobi@users.noreply.github.com>
2018-06-19 01:29:11 +0530

    Add files via upload

---
+++ Transmitter_1.ino
@@ -0,0 +1,24 @@
+#include  <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+int msg[1];
+RF24 radio(9,10);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+int SW1 = 7;
+
+void setup(void){
+ Serial.begin(9600);
+ radio.begin();
+ radio.openWritingPipe(pipe);
+ }
+ 
+ void loop(void)
+ {
+ if (true)
+ {
+ msg[0] = 1;
+ radio.write(msg, 1);
+ Serial.println(msg[0]);
+ delay(10);
+ }
+ }
